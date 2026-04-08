ec16553b112931705a76fe955bf59886388e11fb
Your Name <your.email@example.com>
2026-04-08 19:55:59 +0800

    Add new files

---
+++ js/parking/SortMyParkingArea-data.js
@@ -0,0 +1,13 @@
+// Sort: My Parking Area 游戏数据
+var gameDetailData = {
+    "id": "Sort: My Parking Area",
+    "name": "Sort: My Parking Area",
+    "imageUrl": "img/game_icon/parking/SortMyParkingArea.jpg",
+    "gameType": "Parking",
+    "rating": "87%",
+    "description": "Play Sort: My Parking Area online for free! Test your traffic control skills in this fun and challenging game.",
+    "keywords": "Sort: My Parking Area, traffic control, track game, online game, free game, casual game",
+    "link": "parking/SortMyParkingArea.html",
+    "tags": ["parking", "sort my parking area", "puzzle"],
+    "iframeUrl": "https://html5.gamedistribution.com/4ff5b5355c42484da4fc9e90a970cbe8/?gd_sdk_referrer_url=https://www.onlinegames.io/cat-runner/"
+};
